d1f32e8e7109a78355c30fd43ef5475d98ee732c
Patrick <patrick@imac.home>
2018-01-24 10:50:57 +0000

    Commands

--- src/main/java/com/openplugins/tntrun/TNTRun.java
+++ src/main/java/com/openplugins/tntrun/TNTRun.java
@@ -1,17 +1,26 @@
 package com.openplugins.tntrun;
 
+import com.openplugins.tntrun.command.GameCommands;
 import com.openplugins.tntrun.utils.State;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TNTRun extends JavaPlugin {
 
     private State state;
+    private String version;
+    private GameCommands gameCommands;
+
     private static TNTRun game;
 
     @Override
     public void onEnable() {
         game=this;
         state=State.LOBBY;
+        version="1.0.0";
+
+        gameCommands = new GameCommands();
+
+        getCommand("start").setExecutor(gameCommands);
     }
 
     public static TNTRun getGame() {
@@ -20,5 +29,9 @@ public class TNTRun extends JavaPlugin {
 
     @Override
     public void onDisable() {
+    }
+
+    public String getVersion() {
+        return version;
     }
 }
---
+++ src/main/java/com/openplugins/tntrun/command/GameCommands.java
@@ -0,0 +1,28 @@
+package com.openplugins.tntrun.command;
+
+import com.openplugins.tntrun.TNTRun;
+import com.openplugins.utils.command.SpigotCommand;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+
+public class GameCommands extends SpigotCommand {
+
+    @Override
+    public void executeCommand(Player sender, Command command, String args[]) {
+
+        if (command.getName().equalsIgnoreCase("start")) {
+            if (!sender.hasPermission("tntrun.start") || !sender.hasPermission("openplugins.admin")) {
+                sendPermission(sender);
+                return;
+            }
+
+            // todo start
+        }
+
+        if (command.getName().equalsIgnoreCase("tntrun")) {
+            sender.sendMessage(ChatColor.GREEN + "TNTRun version " + TNTRun.getGame().getVersion());
+        }
+
+    }
+}
